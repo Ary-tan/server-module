925a5537d37cb82e6dcc40d7aaab70e5e90a5680
谭开兵 <kai-bing.tan@reeed.org>
2018-02-08 14:25:37 +0800

    增加静态资源

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static('views/dist'));
 
 app.use('/', index);
 app.use('/users', users);
